c884e8efeda0321c0a1d3f0eb0579ad0b4704112
Emanuelle Natal <emanuellenmoura@gmail.com>
2025-10-02 17:16:37 -0300

    esboço melhor, alinha antes de ver cor cacamba

--- cacamba.py
+++ cacamba.py
@@ -18,17 +18,40 @@ def vetor_cacambas():
         cacambas.append(new_cacamba)
     return cacambas
 
+def orientacao_chao(sensor_cor_dir, sensor_cor_esq, orientacao_estimada):
+    while True:
+        if(sensor_cor_dir.Color() == 'YELLOW' and sensor_cor_esq.Color() == 'YELLOW'):
+            orientacao_estimada = 'L'
+            return
+        elif(sensor_cor_dir.Color() == 'BLUE' and sensor_cor_esq.Color() == 'BLUE'):
+            orientacao_estimada = 'O'
+            return
+        elif((sensor_cor_dir.Color() == 'RED' and sensor_cor_esq.Color() == 'BLUE') or (sensor_cor_dir.Color() == 'YELLOW' and sensor_cor_esq.Color() == 'RED')):
+            orientacao_estimada = 'N'
+            return
+        elif((sensor_cor_dir.Color() == 'RED' and sensor_cor_esq.Color() == 'YELLOW') or (sensor_cor_dir.Color() == 'BLUE' and sensor_cor_esq.Color() == 'RED')):
+            orientacao_estimada = 'S'
+            return
+        elif(sensor_cor_dir.Color() == 'BLUE' or sensor_cor_esq.Color() == 'BLUE'):
+            rodas.turn(30)
+        elif(sensor_cor_dir.Color() == 'YELLOW' and sensor_cor_esq.Color() == 'YELLOW'):
+            rodas.turn(30)
+        elif(sensor_cor_dir.Color() == 'RED' and sensor_cor_esq.Color() == 'RED'):
+            rodas.turn(30)
+        else:
+            rodas.straight()
+
 #começar da linha ate dist cacamba i - dist traseira ate sensor
 def alinhar_cacambas(sensor_cor, orientacao_estimada): 
     '''se coloca virado ao norte, dps
         reto até amarelo vira reto até vermelho, vira vira,
         passar sensor esquerdo'''
+    if (orientacao_estimada == 'O'):
+        virar_esquerda()
+        virar_esquerda()
     if (orientacao_estimada == 'S'):
         virar_esquerda()
-        virar_esquerda()
-    if (orientacao_estimada == 'L'):
-        virar_esquerda()
-    if (orientacao_estimada == 'O'):
+    if (orientacao_estimada == 'N'):
         virar_direita()
     while(sensor_cor.Color() == 'GREEN'):
         rodas.straight()
